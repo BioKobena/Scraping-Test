c41f672ccd276e40f2f276c997d49d0b6f770dfa
BioKobena <biokobena07@gmail.com>
2023-08-29 16:10:54 +0000

    fist commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+import requests as r
+from bs4 import BeautifulSoup
+
+def get_price_information(ticker, exchange):
+    url= f"https://www.google.com/finance/quote/{ticker}/{exchange}"
+    resp = r.get(url)
+    soup = BeautifulSoup(resp.content)
+    
+    
+    
